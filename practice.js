dccd015417fa2173477dd82c733821db18b10dd1
JayAuradkar <80576839+JayAuradkar@users.noreply.github.com>
2021-09-24 15:56:17 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,21 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDPekIvu7hs_z9xujMQPm2QcC1isxvD4Rw",
+    authDomain: "projecc-a8fcb.firebaseapp.com",
+    databaseURL: "https://projecc-a8fcb-default-rtdb.firebaseio.com/",
+    projectId: "projecc-a8fcb",
+    storageBucket: "projecc-a8fcb.appspot.com",
+    messagingSenderId: "623142365263",
+    appId: "1:623142365263:web:2fe4df3b758a22ee6a623a"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  function Click(){ 
+      var username = document.getElementById("username").value
+      var birthday = document.getElementById("birthday").value
+      var mobilenumber = document.getElementById("mobilenumber").value
+      firebase.database().ref("/").child(username).update({
+          purpose:"addingusername"
+      })
+  }
