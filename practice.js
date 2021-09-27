7db4489e36c7ff2b17e8dff7ab680d263dd0cbe2
JayAuradkar <80576839+JayAuradkar@users.noreply.github.com>
2021-09-27 15:32:38 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDPekIvu7hs_z9xujMQPm2QcC1isxvD4Rw",
-    authDomain: "projecc-a8fcb.firebaseapp.com",
-    databaseURL: "https://projecc-a8fcb-default-rtdb.firebaseio.com",
-    projectId: "projecc-a8fcb",
-    storageBucket: "projecc-a8fcb.appspot.com",
-    messagingSenderId: "623142365263",
-    appId: "1:623142365263:web:2fe4df3b758a22ee6a623a"
-  };
-  
-  
-  const app = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDPekIvu7hs_z9xujMQPm2QcC1isxvD4Rw",
+  authDomain: "projecc-a8fcb.firebaseapp.com",
+  databaseURL: "https://projecc-a8fcb-default-rtdb.firebaseio.com",
+  projectId: "projecc-a8fcb",
+  storageBucket: "projecc-a8fcb.appspot.com",
+  messagingSenderId: "623142365263",
+  appId: "1:623142365263:web:2fe4df3b758a22ee6a623a"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
   function Click(){ 
       var username = document.getElementById("username").value
